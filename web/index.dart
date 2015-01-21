89123361d2a6f783ec81eef5d27c03000ed9b017
Thomas Stephenson <ovangle@gmail.com>
2015-01-21 19:51:49 +1100

    Fix for index.dart
    - Removed reference to removed script polymer_utils.dart

--- web/index.dart
+++ web/index.dart
@@ -2,12 +2,12 @@ import 'dart:html';
 
 import 'package:polymer/polymer.dart';
 import 'package:sam_server_web_client/asset_list/asset_list.dart';
-import 'package:sam_server_web_client/polymer_utils.dart';
 import 'package:cs_elements/session/session.dart';
 
 void main() {
   initPolymer().run(() {
-    polymerReady.then((_) {
+    Polymer.onReady.then((_) {
+      var session = document.querySelector('cs-session');
       if (session.loggedIn) {
         AssetList assetList = querySelector('asset-list');
         assetList.loadFromUri();
