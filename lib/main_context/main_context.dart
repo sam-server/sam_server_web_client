3962d9c575836e10448c4799c9d091330750cb93
Thomas Stephenson <ovangle@gmail.com>
2015-01-28 12:26:24 +1100

    Removed lib directory
    Nothing in it was still being used. All elements have been moved
    across to the cs_elements package.
    
    Will move the web directory there too eventually but for the moment
    it can stay put

--- lib/main_context/main_context.dart
+++
@@ -1,7 +0,0 @@
-import 'package:polymer/polymer.dart';
-import 'package:cs_elements/context_panel/context_panel.dart';
-
-@CustomTag('main-context')
-class MainContext extends ContextPanel {
-  MainContext.created(): super.created();
-}
